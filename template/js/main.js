08b7e00eddbaf050b3119bb9c90c25caf87834af
leerivera <leerivera80@gmail.com>
2022-03-17 14:15:56 -0400

    max

--- template/js/main.js
+++ template/js/main.js
@@ -30,12 +30,18 @@
 //     console.log(element)
 // }
 
+// let values = [3, 11, 7,2, 9, 10]
+
+// let sum = 0;
+
+// for(let i =0; i < values.length; i++){
+//     sum = sum + values[i]
+// }
+
+// console.log(sum)
+
 let values = [3, 11, 7,2, 9, 10]
 
-let sum = 0;
+let max = Math.max(...values);
 
-for(let i =0; i < values.length; i++){
-    sum = sum + values[i]
-}
-
-console.log(sum)
+console.log(max)
