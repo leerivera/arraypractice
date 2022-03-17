3cf7474474b05ceca75f41c856e0a2a6ac5f96c1
leerivera <leerivera80@gmail.com>
2022-03-17 10:44:01 -0400

    array

---
+++ template/js/main.js
@@ -0,0 +1,14 @@
+// Write a program that:
+
+// Creates an array named musketeers containing values "Athos", "Porthos" and "Aramis".
+// Shows each array element using a for loop.
+// Adds the "D'Artagnan" value to the array.
+// Shows each array element using the forEach() method.
+// Remove poor Aramis.
+// Shows each array element using a for-of loop.
+
+let musketeers = ["Athos", "Porthos", "Aramis"]
+
+for(let i = 0; i < musketeers.length; i++){
+    console.log(musketeers[i])
+}
