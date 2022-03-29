54effe9825ea6eb79dad5e860888c3019325daba
leerivera <leerivera80@gmail.com>
2022-03-29 12:01:42 -0400

    varient of codewars

--- template/js/main.js
+++ template/js/main.js
@@ -124,3 +124,14 @@ var number = function(busStops){
       busStops.forEach(a => x = x + a[0] - a[1]);
       return x
   }
+
+  var number = function(busStops){
+    var totalPeople = 0;
+    for (var i=0; i<busStops.length; i++) {
+      totalPeople += busStops[i][0];
+      totalPeople -= busStops[i][1];
+    }
+    return totalPeople;
+  }
+
+ 
