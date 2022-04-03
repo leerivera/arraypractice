eb2b01c543cc49fee320fbd3a70d16b760711efb
leerivera <leerivera80@gmail.com>
2022-04-03 12:01:24 -0400

    rock paper

--- template/js/main.js
+++ template/js/main.js
@@ -179,3 +179,17 @@ function sumDigits(number) {
   // Return the product variable
   return product;
 }
+
+const MATCH = {
+  scissors: ['paper', 'lizard'],
+  paper: ['rock', 'spock'],
+  rock: ['lizard', 'scissors'],
+  lizard: ['spock', 'paper'],
+  spock: ['scissors', 'rock'],
+};
+
+
+function rpsls(pl1, pl2) {
+  return MATCH[pl1].indexOf(pl2) >= 0 ? 'Player 1 Won!' :
+         MATCH[pl2].indexOf(pl1) >= 0 ? 'Player 2 Won!' : 'Draw!';
+}
