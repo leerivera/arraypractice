3de0cd819bcfc11215cbc9418172d2c7d0995edc
leerivera <leerivera80@gmail.com>
2022-04-03 12:03:08 -0400

    more rock

--- template/js/main.js
+++ template/js/main.js
@@ -193,3 +193,19 @@ function rpsls(pl1, pl2) {
   return MATCH[pl1].indexOf(pl2) >= 0 ? 'Player 1 Won!' :
          MATCH[pl2].indexOf(pl1) >= 0 ? 'Player 2 Won!' : 'Draw!';
 }
+
+function rpsls(p1,p2){
+  let wins = {
+    scissors:['paper','lizard'],
+    paper:['rock','spock'],
+    rock:['lizard','scissors'],
+    lizard:['spock','paper'],
+    spock:['scissors','rock']
+  }
+  
+  let player1 = wins[p1].includes(p2)
+  let player2 = wins[p2].includes(p1)
+  
+  if (!player1 && !player2) { return 'Draw!' } 
+  return `Player ${ player1?'1':'2'} Won!`
+}
